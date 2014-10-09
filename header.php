00e6c757ccf67ffa8bf92db4e6ad39766c80497f
Andrew Ek <andrewek@gmail.com>
2014-10-09 13:38:50 -0500

    'Functioning' static site; now to make it dynamic

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title><?php wp_title(); ?> <?php bloginfo('name'); ?> </title>
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen">
+  <!--><link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen"> -->
   
   <?php wp_head(); ?>
   
