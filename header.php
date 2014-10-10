47e9a624fe72b0dec420ffc0f9c22629e84cc11d
Andrew Ek <andrewek@gmail.com>
2014-10-10 11:13:11 -0500

    More splitting into templates

--- header.php
+++ header.php
@@ -22,3 +22,21 @@
       <img src="http://www.omahafilmfestival.org/images/index_r4_c1.jpg" class="banner-image">
       </div>
     </div>
+    <!-- Start to the body content -->
+    <div class="body-wrapper clearfix">
+    <?php get_sidebar() ?>
+    <!-- Main Body Content -->
+    <div class="main-body">
+      <div class= "title-banner clearfix">
+        <div class= "page-title">
+          <?php $title = single_term_title('', false) ?>
+          <?php if (gettype( $title )=="string" ): ?>
+            <h1><?php echo $title ?></h1>
+          <?php else: ?>
+            <h1><?php the_title() ?></h1>
+          <?php endif ?>
+        </div>
+        <div class= "social-media">
+          <h2>Follow | t | f</h2>
+        </div>
+      </div>
--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php get_header() ?>
     
-<!-- Start to the body content -->
-<div class="body-wrapper clearfix">
-<?php get_sidebar() ?>
-<!-- Main Body Content -->
-<div class="main-body">
-  <div class= "title-banner clearfix">
-    <div class= "page-title">
-      <h1><?php the_title() ?></h1>
-    </div>
-    <div class= "social-media">
-      <h2>Follow | t | f</h2>
-    </div>
-  </div>
   <div class= "main-body-content">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
       <p><?php the_content(__('(more...)')); ?></p>
