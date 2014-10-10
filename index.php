65e63746510cd3e3b19317c03eef2ba311038e95
Andrew Ek <andrewek@gmail.com>
2014-10-10 10:29:40 -0500

    Functional page titles

--- index.php
+++ index.php
@@ -17,46 +17,45 @@
       <!-- Top Navigation Bar -->
       <div class="nav-bar border-top border-bottom centered">
         <?php if( has_nav_menu( 'top-nav' ) ) { /* if menu location 'primary-menu' exists then use custom menu */
-              wp_nav_menu( array( 'theme_location' => 'top-nav', 'menu_class' => 'top-nav') ); 
-          }
-        ?>
-        <img src="http://www.omahafilmfestival.org/images/index_r4_c1.jpg" class="banner-image">
+        wp_nav_menu( array( 'theme_location' => 'top-nav', 'menu_class' => 'top-nav') ); 
+      }
+      ?>
+      <img src="http://www.omahafilmfestival.org/images/index_r4_c1.jpg" class="banner-image">
+    </div>
+  </div>
+    
+  <!-- Start to the body content -->
+  <div class="body-wrapper clearfix">
+    <!-- Navigation Sidebar -->
+    <div class="side-nav sidebar">
+      <?php if( has_nav_menu( 'side-nav' ) ) { /* if menu location 'primary-menu' exists then use custom menu */
+      wp_nav_menu( array( 'theme_location' => 'side-nav', 'menu_class' => 'side-nav') ); 
+    }
+    ?>
+  </div>
+  <!-- Main Body Content -->
+  <div class="main-body">
+    <div class= "title-banner clearfix">
+      <div class= "page-title">
+        <?php $title = single_term_title('', false) ?>
+        <?php if (gettype( $title )=="string" ): ?>
+          <h1><?php echo $title ?></h1>
+        <?php else: ?>
+          <h1><?php the_title() ?></h1>
+        <?php endif ?>
+      </div>
+      <div class= "social-media">
+        <h2>Follow | t | f</h2>
       </div>
     </div>
-    
-
-    
-    <?//php wp_nav_menu( array( 'theme_location' => 'top-nav' ) ); ?>
-    
-    <!-- Start to the body content -->
-    <div class="body-wrapper clearfix">
-      <!-- Navigation Sidebar -->
-      <div class="side-nav sidebar">
-        <?php if( has_nav_menu( 'side-nav' ) ) { /* if menu location 'primary-menu' exists then use custom menu */
-              wp_nav_menu( array( 'theme_location' => 'side-nav', 'menu_class' => 'side-nav') ); 
-          }
-        ?>
-        <ul>
-          <li class="side-nav"><a href="#" class="nav-item">Showtimes</a></li>
-          <li class="side-nav"><a href="#" class="nav-item">Upcoming Events</a></li>
-          <li class="side-nav"><a href="#" class="nav-item">Conferences</a></li>
-          <li class="side-nav"><a href="#" class="nav-item">Past Events</a></li>
-          <li class="side-nav"><a href="#" class="nav-item">Screenplay Finalists</a></li>
-          <li class="side-nav"><a href="#" class="nav-item">Festival Passes</a></li>
-        </ul>
-      </div>
-      <!-- Main Body Content -->
-      <div class="main-body">
-        <div class= "title-banner clearfix">
-          <div class= "page-title">
-            <h1>Welcome</h1>
-          </div>
-          <div class= "social-media">
-            <h2>Follow | t | f</h2>
-          </div>
-        </div>
-        <div class= "main-body-content">
-          <p>Sample text</p>
+    <div class= "main-body-content">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <p><?php the_content(__('(more...)')); ?></p>
+        <hr> 
+      <?php endwhile; else: ?>
+        <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+      <?php endif; ?>
         </div>
       </div>
       <!-- Events sidebar -->
