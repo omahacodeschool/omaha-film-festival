618772ddfe61bb47503a9863fc28be96e905c1fa
Andrew Ek <andrewek@gmail.com>
2014-10-14 14:39:56 -0500

    Dynamic Header / Footer, pt. 1

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title><?php wp_title(); ?> <?php bloginfo('name'); ?> </title>
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-  
+  <title><?php wp_title(); ?> <?php bloginfo('name'); ?> </title>  
   <?php wp_head(); ?>
+  
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
   
 </head>
 <body>
@@ -12,14 +12,15 @@
     <!-- Header content & Navbar -->
     <div class="header">
       <div class="banner">
-        <img src="http://www.omahafilmfestival.org/images/index_r1_c1.jpg" class="banner-image">
+        <img src="<?php bloginfo('template_directory'); ?>/images/default_header.jpg" class="banner-image" >
+        <!-- <img src="http://www.omahafilmfestival.org/images/index_r1_c1.jpg" class="banner-image"> -->
       </div>
       <!-- Top Navigation Bar -->
-      <div class="nav-bar border-top border-bottom centered">
+      <div class="nav-bar border-top-and-bottom centered">
         <?php if( has_nav_menu( 'top-nav' ) ) { /* if menu location 'primary-menu' exists then use custom menu */
         wp_nav_menu( array( 'theme_location' => 'top-nav', 'menu_class' => 'top-nav') ); 
       } ?>
-      <img src="http://www.omahafilmfestival.org/images/index_r4_c1.jpg" class="banner-image">
+      <!-- <img src="http://www.omahafilmfestival.org/images/index_r4_c1.jpg" class="banner-image"> -->
       </div>
     </div>
     <!-- Start to the body content -->
