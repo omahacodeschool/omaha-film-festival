0437dd964fb81ad377cc60135ab6f7a329a2ba68
Sumeet Jain <sumeet@sumeetjain.com>
2014-10-17 11:35:24 -0500

    Use normal font instead of bold

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   <?php wp_head(); ?>
   <script type="text/javascript" src="<?php bloginfo("template_url"); ?>/js/columnheight.js"></script>
   
-  <link href='http://fonts.googleapis.com/css?family=PT+Sans:700|Rokkitt:700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans|Rokkitt:700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
   
 </head>
